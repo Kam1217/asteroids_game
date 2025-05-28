06b707e08c38b767b6ba55048e5d5f2097c3295f
Kam1217 <kamilaostroga@gmail.com>
2025-05-28 08:35:41 +0100

    Add asteroids group into main

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     updatable_group = pygame.sprite.Group()
     drawable_group = pygame.sprite.Group()
+    asteroids_group = pygame.sprite.Group()
+    
     Player.containers = (updatable_group, drawable_group)   
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    Asteroid.containers = (updatable_group, drawable_group, asteroids_group)
     
     while True:
        for event in pygame.event.get():
