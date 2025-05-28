6934ffecda4b5379534a9b15618f392304b7ea27
Kam1217 <kamilaostroga@gmail.com>
2025-05-28 09:20:45 +0100

    Shot file that contains shot class

---
+++ shot.py
@@ -0,0 +1,15 @@
+import pygame
+from circleshape import CircleShape
+
+class Shot(CircleShape):
+     def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+     
+     def draw(self, screen):
+        pass
+    
+     def update(self, dt):
+        pass
+     
+     def collides_with(self, other):
+        pass
