f2450421ec1022efd17d8971c9072e8c7ebe141a
Kam1217 <kamilaostroga@gmail.com>
2025-05-28 12:27:52 +0100

    Asteroids split when hit with a bullet

--- asteroid.py
+++ asteroid.py
@@ -14,16 +14,19 @@ class Asteroid(CircleShape):
         self.position += self.velocity * dt
 
     def split(self):
+        print("Split method called!")
         self.kill()
         if self.radius <= ASTEROID_MIN_RADIUS:
+            print("Small asteroid - returning early")
             return
-        random_angle = random.uniform(20, 50)
-        new_velocity1 = self.velocity.rotate(random_angle)
-        new_velocity2 = self.velocity.rotate(-random_angle) 
-
         small_asteroid_radius = self.radius - ASTEROID_MIN_RADIUS
+        print(f"Creating new asteroids with radius: {small_asteroid_radius}")
+        print(f"Current asteroid position: {self.position}")
+        random_angle = random.uniform(20, 50)
+        new_velocity1 = self.velocity.rotate(random_angle) * 1.2
+        new_velocity2 = self.velocity.rotate(-random_angle) * 1.2
 
-        new_asteroid = Asteroid(new_velocity1, self.position,small_asteroid_radius)
-        new_asteroid.velocity *= 1.2
-        new_asteroid2 = Asteroid(new_velocity2, self.position,small_asteroid_radius) 
-        new_asteroid2.velocity *= 1.2
+        new_asteroid = Asteroid(self.position.x, self.position.y, small_asteroid_radius) 
+        new_asteroid.velocity = new_velocity1
+        new_asteroid2 = Asteroid(self.position.x, self.position.y, small_asteroid_radius) 
+        new_asteroid2.velocity = new_velocity2
